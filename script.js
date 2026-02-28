6c3d19c83d4b6f18f7221e22f0b389bb0b7d2e23
Bryne <228299496+b1yn3@users.noreply.github.com>
2026-02-28 15:38:21 +0800

    refactor: Encapsulate game logic into createGame function

--- script.js
+++ script.js
@@ -1,41 +1,57 @@
-const buttons = document.querySelectorAll('.parent button');
-let currentPlayer = "X";
-const board = Array(9).fill(null);
+function createGame(){
+    const buttons = document.querySelectorAll('.parent button');
+    let currentPlayer = "X";
+    const board = Array(9).fill(null);
 
-buttons.forEach((button, index) => {
-    button.addEventListener('click', function () {
-        board[index] = currentPlayer;
-        this.disabled = true;
-        if (currentPlayer === "O") {
-            this.style.color = "#2a9daf";
-        } else {
-            this.style.color = "#e76f51";
+    function checkForWin() {
+        const winList = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        for(const win of winList){
+            if(board[win[0]] === currentPlayer && board[win[1]] === currentPlayer && board[win[2]] === currentPlayer){
+                console.log("WIN");
+            }
         }
-        this.textContent = currentPlayer;
-        checkForWin(currentPlayer);
-        currentPlayer = currentPlayer === "X" ? "O" : "X";
-    });
 
-});
-
-
-function checkForWin(currentPlayer) {
-    const winList = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
-
-    for(const win of winList){
-        if(board[win[0]] === currentPlayer && board[win[1]] === currentPlayer && board[win[2]] === currentPlayer){
-            console.log("WIN");
-        }
     }
 
+    function handleMove(button, index){
+        board[index] = currentPlayer;
+        if(currentPlayer === "X") {
+            button.style.color = "#2a9daf";
+        } else {
+            button.style.color = "#e76f51";
+        }
+        button.textContent = currentPlayer;
+        button.disabled = true;
+
+        checkForWin();
+
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+    }
+
+    function startGame(){
+        buttons.forEach((button, index) => {
+        button.addEventListener('click', function () {
+            handleMove(button, index);
+        });
+
+    });
+    }
+
+    return {
+        startGame
+    };
 }
+
+const game = createGame();
+game.startGame();
 
